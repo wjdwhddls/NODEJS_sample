904d574c0ad82663c7127e407a078d533450cf03
jjong_0425 <bfpark99@naver.com>
2025-01-08 12:40:01 +0900

    타입스크립트 파일 생성 및 컴파일, 명시적 타입 선언 방법

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-console.log("Hello")
-
-let a = 10;  //숫자, number
-let b = "Hello"; //문자열, string
-let c = true; // 참,거짓 boolean
-let d = [1,2,3,4]; //배열, array
-let e = {name : "Tom", age : 35}; //객체, object
-
-
+console.log("Hello");
+var a = 10; //숫자, number
+var b = "Hello"; //문자열, string
+var c = true; // 참,거짓 boolean
+var d = [1, 2, 3, 4]; //배열, array
+var e = { name: "Tom", age: 35 }; //객체, object
 //..
-b = 50;
+a = 50;
+b = "50";
 //..
-
-
-console.log("a= "+a,  "type of= "+ typeof a)
-console.log("b= "+b,  "type of="+ typeof b)
-console.log("c= "+c,  "type of= "+ typeof c)
-console.log("d= "+d,  "typeof"+ typeof d)
-console.log("d[0]= "+d[0],  "type of= "+ typeof d)
-console.log("e.name= "+e.name,"e.age= "+e.age , "type of= "+ typeof e)
-
+console.log("a= " + a, "type of= " + typeof a);
+console.log("b= " + b, "type of=" + typeof b);
+console.log("c= " + c, "type of= " + typeof c);
+console.log("d= " + d, "typeof" + typeof d);
+console.log("d[0]= " + d[0], "type of= " + typeof d);
+console.log("e.name= " + e.name, "e.age= " + e.age, "type of= " + typeof e);
 //예시
-
-let columnLength = 10; //길이를 number로 지정
+var columnLength = 10; //길이를 number로 지정
 //..개발 진행 과정
 //..개발 진행 과정
 //..개발 진행 과정
@@ -31,15 +25,13 @@ let columnLength = 10; //길이를 number로 지정
 //..개발 진행 과정
 //..개발 진행 과정
 //..개발 진행 과정
-columnLength = "30cm";
-
+columnLength = 30;
 //..개발 진행 과정
 //..개발 진행 과정
 //..개발 진행 과정
 //..개발 진행 과정
-let addLength = columnLength + 5;
-let meterLength = addLength / 10;
-
+var addLength = columnLength + 5;
+var meterLength = addLength / 10;
 //..개발 진행 과정
 //..개발 진행 과정
 //..개발 완료
---
+++ app.ts
@@ -0,0 +1,51 @@
+console.log("Hello")
+
+let a:number = 10;  //숫자, number
+let b:string = "Hello"; //문자열, string
+let c:boolean = true; // 참,거짓 boolean
+let d:number[] = [1,2,3,4]; //배열, array
+let e:Person = {name : "Tom", age : 35}; //객체, object
+
+interface Person{
+    name:string;
+    age:number;
+}
+//..
+a = 50;
+b = "50";
+//..
+
+
+console.log("a= "+a,  "type of= "+ typeof a)
+console.log("b= "+b,  "type of="+ typeof b)
+console.log("c= "+c,  "type of= "+ typeof c)
+console.log("d= "+d,  "typeof"+ typeof d)
+console.log("d[0]= "+d[0],  "type of= "+ typeof d)
+console.log("e.name= "+e.name,"e.age= "+e.age , "type of= "+ typeof e)
+
+//예시
+
+let columnLength = 10; //길이를 number로 지정
+//..개발 진행 과정
+//..개발 진행 과정
+//..개발 진행 과정
+//..개발 진행 과정
+//..개발 진행 과정
+//..개발 진행 과정
+//..개발 진행 과정
+//..개발 진행 과정
+//..개발 진행 과정
+columnLength = 30;
+
+//..개발 진행 과정
+//..개발 진행 과정
+//..개발 진행 과정
+//..개발 진행 과정
+let addLength = columnLength + 5;
+let meterLength = addLength / 10;
+
+//..개발 진행 과정
+//..개발 진행 과정
+//..개발 완료
+console.log(addLength);
+console.log(meterLength);
