8bd064b72bc2243c11cc5277dfd95f28cca67697
jjong_0425 <bfpark99@naver.com>
2025-01-08 16:39:26 +0900

    배열 타입

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,3 +8,29 @@ console.log(str.toUpperCase()); // HELLO
 
 //String, Number, Boolean 는 타입 명시법과 다르니 주의
 const wrapperStr = new String(num); //강제 형변환
+
+
+//Array
+//-----------------배열의 초기화 방법------------------------------------
+let arrayOfNumbers1:number[];    
+arrayOfNumbers1 = [5];
+arrayOfNumbers1 = [5,10,30];
+//[5], [5,5,10,30] XXX
+//[5.10.30] OOO
+
+console.log(arrayOfNumbers1[0]); // 5
+console.log(arrayOfNumbers1[1]); // 10
+console.log(arrayOfNumbers1[2]); // 30
+
+
+let arrayOfNumbers2:Array<number>;  //잘안씀
+arrayOfNumbers2 = [6];
+arrayOfNumbers2 = [6,8,20];
+
+console.log(arrayOfNumbers2[2]); // 20
+
+// 해당 타입 요소만 허용되는 배열 = 튜플이므로 선언 주의
+//let singleNumberTuple: [number];
+//singleNumberTuple = [5];
+
+
