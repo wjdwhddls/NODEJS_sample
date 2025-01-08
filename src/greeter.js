4ca0c41511866cda1f3047495956679cc171a0bd
jjong_0425 <bfpark99@naver.com>
2025-01-08 15:34:17 +0900

    Greeter 타입 스크립트 소개

---
+++ src/greeter.js
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "정종인";
+console.log(greeter(user));
+//document.body.textContent = greeter(user);
---
+++ src/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person:string,){
+    return "Hello, " + person;
+}
+
+let user = "정종인";
+console.log(greeter(user));
+//document.body.textContent = greeter(user);
