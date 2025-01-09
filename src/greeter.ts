3f561d730032f690b4c1afb8034260e50e149b0d
jjong_0425 <bfpark99@naver.com>
2025-01-09 10:14:04 +0900

    함수의 타입지정 1

--- src/greeter.ts
+++ src/greeter.ts
@@ -1,7 +1,7 @@
-function greeter(person:string,){
+function greeter1(person:string,){
     return "Hello, " + person;
 }
 
 let user = "정종인";
-console.log(greeter(user));
+console.log(greeter1(user));
 //document.body.textContent = greeter(user);
