2d1022b5baf2f645a1b3de02860f130186d4116d
jjong_0425 <bfpark99@naver.com>
2025-01-09 15:51:20 +0900

    클래스 사용 양식

--- src/class/class.ts
+++ src/class/class.ts
@@ -47,3 +47,34 @@ class Pet{
 
 let p1 = new Pet();
 console.log(p1.name);
+
+class User{
+    //[필드 부분]
+    username: string; //타입만을 표기하는 경우
+    email: string;
+    job: string = "Student"; //기본값을 주는 경우
+
+    //[생성자 부분]
+    constructor(username: string, email: string){
+        this.username = username; //매개변수로 초기화
+        this.email = email;
+    }
+
+    //[메서드 부분]
+    study(): void{
+        console.log(`${this.username} is studying`);
+    }
+}
+
+//[객체 생성] (new 키워드와 arguments 입력)
+let user1 = new User("홍길동", "hong@afdsf.co.kr");
+let user2 = new User("김철수", "kim@afdsf.co.kr");
+
+//[객체의 사용,접근] (. 도트연산자 사용)
+//1. 인스턴스 자체 접근
+console.log(user1) //출력: User{username: '홍길동', email: '...'}
+//2. 인스턴스의 세부 필드 접근
+console.log(user2.username); //출력: 김철수
+//3. 메서드 호출
+user1.study();
+user2.study();
